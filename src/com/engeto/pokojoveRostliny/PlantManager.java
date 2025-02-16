a2d8f6ae80a757a31cd5cfe8764946c9e6625e7e
FilipUrbanek406 <filipurbanek406@gmail.com>
2025-02-16 16:05:10 +0100

    Zpracované úkoly 1, 2, 3, 4 (nejsem si vším jistý)

---
+++ src/com/engeto/pokojoveRostliny/PlantManager.java
@@ -0,0 +1,39 @@
+package com.engeto.pokojoveRostliny;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlantManager {
+
+    //Nejsem si tím atributem ještě jistý
+    private List<Plant> plants = new ArrayList<>();
+
+    public void addPlant(Plant plant) {
+        plants.add(plant);
+    }
+
+    public void removePlant(Plant plant) {
+        plants.remove(plant);
+    }
+
+    public Plant getPlantOfIndex(int index) {
+        return plants.get(index);
+    }
+
+    //získání kopie seznamu rostlin - nejsem si jistý, zda je to správně
+    public List<Plant> getPlants() {
+        return new ArrayList<>(plants);
+    }
+
+    //metoda, která má vrátit seznam rostlin, které je třeba zalít (datum zálivky je starší než počet dnů kdy mají být zalité)
+    public List<Plant> getPlantsToWater() {
+        List<Plant> plantsToWater = new ArrayList<>();
+        for (Plant plant : plants) {
+            if (plant.getWatering().isBefore(LocalDate.now().minusDays(plant.getFrequencyOfWatering()))) {
+                plantsToWater.add(plant);
+            }
+        }
+        return plantsToWater;
+    }
+}
